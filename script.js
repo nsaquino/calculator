4532eec373813d596001570acc03109a8de6b5bc
Nicolas Aquino <aquino_nicolas@hotmail.com>
2023-09-25 16:18:58 -0300

    Add 'operate' function

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ function divide(a, b) {
     return a / b;
 }
 
+//---
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return substract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+}
+
+//---
+
+let firstNum;
+let secondNum;
+let operator;
+
